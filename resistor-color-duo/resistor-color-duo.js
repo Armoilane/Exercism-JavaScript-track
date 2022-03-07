d5de1004a95b9f57406e7533260e44d6da64aeee
Ari Moilanen <armo@iki.fi>
2022-03-07 13:21:37 +0200

    Add resistor-color-duo - Iteration 3
    No more depending on imports.
    
    Constant `COLORS` to hold the band colors.
    
    Defined a helper method `colorCode` to find the number code for the
    color of the band at hand.

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,6 +1,19 @@
-import { colorCode } from '../resistor-color/resistor-color';
+const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white'
+]
+
+const colorCode = band => COLORS.indexOf(band)
 
 export const decodedValue = (colors) => {
   return colors.slice(0,2).reduce((total, color) =>
-    total * 10 + colorCode(color), 0 );
+                                   total * 10 + colorCode(color), 0 );
 };
