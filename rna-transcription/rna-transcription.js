672689f238a7ecb1d39741aff3231f7e6fd60330
Ari Moilanen <armo@iki.fi>
2022-03-09 10:33:10 +0200

    Add rna-transcription - Iteration 6
    Rename the constant that holds the regular expression to match the
    nucleotides.

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,5 +1,5 @@
 const TRANSCRIPTION = { G: 'C', C: 'G', T: 'A', A: 'U' }
-const RX_NUCLEOTIDES = new RegExp(Object.keys(TRANSCRIPTION).join('|'), 'g')
+const NUCLEOTIDE = new RegExp(Object.keys(TRANSCRIPTION).join('|'), 'g')
 
 export const toRna = strand =>
-  strand.replace(RX_NUCLEOTIDES, nucleotide => TRANSCRIPTION[nucleotide])
+  strand.replace(NUCLEOTIDE, nucleotide => TRANSCRIPTION[nucleotide])
