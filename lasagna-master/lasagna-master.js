0ef21e1e956b0db59b8c56dd58ff2f630013dee4
Ari Moilanen <armo@iki.fi>
2022-03-19 16:27:04 +0200

    Add lasagna-master - Iteration 1

--- lasagna-master/lasagna-master.js
+++ lasagna-master/lasagna-master.js
@@ -10,3 +10,40 @@
  *   ...
  * }
  */
+export function cookingStatus(remainingTime) {
+  if (remainingTime === 0) {
+    return 'Lasagna is done.'
+  }
+  if (!remainingTime) {
+    return 'You forgot to set the timer.'
+  }
+  return 'Not done, please wait.'
+}
+
+export function preparationTime(layers, timePerLayer = 2) {
+  return layers.length * timePerLayer;
+}
+
+function sauceAndNoodles(layers) {
+  return layers.filter(layer => layer === 'sauce' || layer === 'noodles');
+}
+
+export function quantities(layers) {
+  const unitsPerLayer = { noodles: 50, sauce: 0.2 }
+  let quantitiesByLayer = { noodles: 0, sauce: 0 }
+  sauceAndNoodles(layers).forEach(layer =>
+    quantitiesByLayer[layer] += unitsPerLayer[layer])
+  return quantitiesByLayer
+}
+
+export function addSecretIngredient(listFrom, listTo) {
+  listTo.push(listFrom[listFrom.length - 1])
+}
+
+export function scaleRecipe(recipe, portions) {
+  let scaled = {}
+  for (const ingredient in recipe) {
+    scaled[ingredient] = recipe[ingredient] * portions / 2
+  }
+  return scaled
+}
