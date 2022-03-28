27c6e0dfc71830acffab77e873adaf8746258737
Ari Moilanen <armo@iki.fi>
2022-03-28 08:35:41 +0300

    Add elyses-destructured-enchantments - Iteration 1
    Introducing the `...` as `rest` and `spread` operator for array
    destructuring.
    
    Not the cleanest solutions.

--- elyses-destructured-enchantments/enchantments.js
+++ elyses-destructured-enchantments/enchantments.js
@@ -9,7 +9,8 @@
  * @returns {Card} the first card in the deck
  */
 export function getFirstCard(deck) {
-  throw new Error('Implement the getFirstCard function');
+  let [firstCard, ...rest] = deck
+  return firstCard
 }
 
 /**
@@ -20,7 +21,8 @@ export function getFirstCard(deck) {
  * @returns {Card} the second card in the deck
  */
 export function getSecondCard(deck) {
-  throw new Error('Implement the getSecondCard function');
+  let [firstCard, secondCard, ...rest] = deck
+  return secondCard
 }
 
 /**
@@ -31,7 +33,9 @@ export function getSecondCard(deck) {
  * @returns {Card[]} new deck with reordered cards
  */
 export function swapTopTwoCards(deck) {
-  throw new Error('Implement the swapTopTwoCards function');
+  let [firstCard, secondCard, ...rest] = deck
+  let topTwoCardsSwapped = [secondCard, firstCard, ...rest]
+  return topTwoCardsSwapped
 }
 
 /**
@@ -43,7 +47,8 @@ export function swapTopTwoCards(deck) {
  * deck and a new deck containing all the other cards
  */
 export function discardTopCard(deck) {
-  throw new Error('Implement the discardTopCard function');
+  let [firstCard, ...rest] = deck
+  return [firstCard, [...rest]]
 }
 
 /** @type Card[] **/
@@ -58,5 +63,6 @@ const FACE_CARDS = ['jack', 'queen', 'king'];
  * third, and fourth cards are the face cards
  */
 export function insertFaceCards(deck) {
-  throw new Error('Implement the insertFaceCards function');
+  let [firstCard, ...rest] = deck
+  return [firstCard, ...FACE_CARDS, ...rest]
 }
