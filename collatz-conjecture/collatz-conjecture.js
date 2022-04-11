8eba2d34ddd9c8f316f4b3c641bf2e129ac8a29c
Ari Moilanen <armo@iki.fi>
2022-04-11 17:22:55 +0300

    Add collatz-conjecture - Iteration 4
    Jumped to the recursive bandwagon.
    
    Now we throw the number back into `steps` after doing the conjecture on
    the number. Count is declared as a parameter with default value and
    increased on every cycle. Once n reaches 1 it returns the count.
    
    It's pretty sweet. I didn't know I could cycle something through one
    function like this.

--- collatz-conjecture/collatz-conjecture.js
+++ collatz-conjecture/collatz-conjecture.js
@@ -1,25 +1,16 @@
 /**
  * Collatz Conjecture.
  *
- * @param {Number} number any positive integer
+ * @param {Number} n any positive integer
  * @returns {Number} number of cycles it took to get to 1
  */
-export const steps = number => {
-  if (number < 1) {
+export const steps = (n, count = 0) => {
+  if (n < 1) {
     throw new Error('Only positive numbers are allowed')
   }
-
-  let num = number
-  let steps = 0
-
-  while (num > 1) {
-    if (num % 2 === 0) {
-      num /= 2
-    } else {
-      num = 3 * num + 1
-    }
-    steps++
+  if (n === 1) {
+    return count
   }
 
-  return steps
+  return steps(n % 2 === 0 ? n / 2 : n * 3 + 1, count + 1)
 }
