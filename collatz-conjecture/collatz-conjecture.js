6cbd50bd84f51eb244d8b0e89c6230dbbc341470
Ari Moilanen <armo@iki.fi>
2022-04-11 12:43:36 +0300

    Add collatz-conjecture - Iteration 2
    Move the `throw` to be the first action in the function.
    
    Clean the syntax.

--- collatz-conjecture/collatz-conjecture.js
+++ collatz-conjecture/collatz-conjecture.js
@@ -1,20 +1,29 @@
-
-export const steps = (number) => {
-  let num = number;
-  let steps = 0;
-  if ( num < 1 ) {
-    throw Error("Only positive numbers are allowed");
+/**
+ * Take an integer.
+ * Throw if it isn't positive by nature.
+ * If it's even, divide by 2
+ * If it's odd, multiply by 3 and add 1
+ * Count the number of cycles for the n to reach 1
+ * @param {Number} number any positive integer
+ * @returns {Number} number of cycles it took to get to 1
+ */
+export const steps = number => {
+  if (number < 1) {
+    throw Error("Only positive numbers are allowed")
   }
 
-  do {
-    if ( num % 2 === 0 ) {
-      num = num / 2;
-      steps++;
-    } else if ( num > 2 ) {
-      num = 3*num + 1;
-      steps++;
-    }
-  } while (num != 1);
+  let num = number
+  let steps = 0
 
-  return steps;
-};
+  do {
+    if (num % 2 === 0) {
+      num = num / 2
+      steps++
+    } else if (num > 2) {
+      num = 3 * num + 1
+      steps++
+    }
+  } while (num > 1)
+
+  return steps
+}
