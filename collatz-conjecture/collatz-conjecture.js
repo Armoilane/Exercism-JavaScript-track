09cd7315da55d12badc454dfd3f0e2ec76110d5b
Ari Moilanen <armo@iki.fi>
2022-04-11 16:46:22 +0300

    Add collatz-conjecture - Iteration 3
    Ditched the `do...while` for `while` and it cleaned up the loop.

--- collatz-conjecture/collatz-conjecture.js
+++ collatz-conjecture/collatz-conjecture.js
@@ -1,29 +1,25 @@
 /**
- * Take an integer.
- * Throw if it isn't positive by nature.
- * If it's even, divide by 2
- * If it's odd, multiply by 3 and add 1
- * Count the number of cycles for the n to reach 1
+ * Collatz Conjecture.
+ *
  * @param {Number} number any positive integer
  * @returns {Number} number of cycles it took to get to 1
  */
 export const steps = number => {
   if (number < 1) {
-    throw Error("Only positive numbers are allowed")
+    throw new Error('Only positive numbers are allowed')
   }
 
   let num = number
   let steps = 0
 
-  do {
+  while (num > 1) {
     if (num % 2 === 0) {
-      num = num / 2
-      steps++
-    } else if (num > 2) {
+      num /= 2
+    } else {
       num = 3 * num + 1
-      steps++
     }
-  } while (num > 1)
+    steps++
+  }
 
   return steps
 }
