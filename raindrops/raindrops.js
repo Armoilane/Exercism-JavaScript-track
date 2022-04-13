f01da5a2a01a7c151c6fa330400a8e57a580779e
Ari Moilanen <armo@iki.fi>
2022-04-13 15:20:00 +0300

    Add raindrops - Iteration 2
    Indeed we dont need that const or that `if..` clause.
    
    We can return with `||` either the string or the number.

--- raindrops/raindrops.js
+++ raindrops/raindrops.js
@@ -9,12 +9,10 @@ const SOUNDS = {
  * @param {Number} n
  * @returns {String} Pling, Plang, Plong
  */
-export const convert = (n) => {
-  const nfactors = Object.keys(SOUNDS).filter(factor => n % factor === 0)
-
-  if (nfactors.length > 0) {
-    return nfactors.map(factor => SOUNDS[factor]).join('')
-  }
-
-  return n.toString()
+export const convert = n => {
+  return Object.keys(SOUNDS)
+    .filter(factor => n % factor === 0)
+    .map(factor => SOUNDS[factor])
+    .join('')
+    || n.toString()
 }
