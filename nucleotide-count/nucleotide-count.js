172d6468ffa6dfbb6f6d073a9b8cd4658b61ae06
Ari Moilanen <armo@iki.fi>
2022-05-04 08:16:22 +0300

    Add nucleotide-count - Iteration 1
    The constants make things easier though not necessary. If not all
    nucleotides are valid, an error is thrown. If not, they're mapped
    through and their counts are incremented in the COUNTS object. To get a
    string of the counts returned, the values are joined.

--- nucleotide-count/nucleotide-count.js
+++ nucleotide-count/nucleotide-count.js
@@ -1,8 +1,20 @@
-//
-// This is only a SKELETON file for the 'Nucleotide Count' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
+/**
+ * Count nucleotides in a gived strand of RNA
+ * @param {String} strand
+ * @returns {String} The counts separated with spaces '0 0 0 0'
+ */
 export function countNucleotides(strand) {
-  throw new Error('Remove this statement and implement this function');
+  const VALIDNUCLEOTIDES = ['A', 'C', 'G', 'T']
+  const COUNTS = {A: 0, C: 0, G: 0, T: 0}
+  const nucleotides = strand.split('')
+
+  if (!nucleotides.every(n => VALIDNUCLEOTIDES.includes(n))) {
+    throw new Error('Invalid nucleotide in strand')
+  }
+
+  nucleotides.map(nucleotide => COUNTS[nucleotide]++)
+
+  return Object.values(COUNTS).join(' ')
+
 }
+
