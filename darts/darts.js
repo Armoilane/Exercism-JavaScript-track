c888543687d7d489603e2caab84cf2d8600aa6d6
Ari Moilanen <armo@iki.fi>
2022-05-25 16:28:27 +0300

    Add darts - Iteration 1

--- darts/darts.js
+++ darts/darts.js
@@ -1,8 +1,15 @@
-//
-// This is only a SKELETON file for the 'Darts' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+/**
+ * Darts scoring
+ * @param {Number} x x distance from center
+ * @param {Number} y y distance from center
+ * @returns {Number} score
+ */
+export const score = (x, y) => {
+  let distance = Math.sqrt(x**2 + y**2)
 
-export const score = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+  if (distance <= 1) return 10
+  if (distance <= 5) return 5
+  if (distance <= 10) return 1
+  return 0
+}
+
