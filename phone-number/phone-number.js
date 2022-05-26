f111e4281fff87cfa283877432694192abdea8d1
Ari Moilanen <armo@iki.fi>
2022-05-26 07:22:25 +0300

    Add phone-number - Iteration 2
    Instead of that `if` hell in iteration one, this is pretty clean!
    
    In `VALIDATIONS` we have and array of arrays holding pairs of function
    followed by an error message.
    
    In function `clean` we first process the input by replacing
    non-numericals with nothing using `replace`. Then it's normalized by
    adding the country code '1' to the beginning if needed.
    
    Iterating through `VALIDATIONS` checks if an error condition is met and
    if it is it throws an error and if not the number is returned without
    the NA country code '1'.

--- phone-number/phone-number.js
+++ phone-number/phone-number.js
@@ -1,51 +1,29 @@
+const VALIDATIONS = [
+  [phoneNumber => phoneNumber.length < 11, 'Incorrect number of digits'],
+  [phoneNumber => phoneNumber.length > 11, 'More than 11 digits'],
+  [phoneNumber => phoneNumber.match(/[a-z]/gi), 'Letters not permitted'],
+  [phoneNumber => !phoneNumber.match(/\d{11}/), 'Punctuations not permitted'],
+  [phoneNumber => phoneNumber[0] !== '1', '11 digits must start with 1'],
+  [phoneNumber => phoneNumber[1] === '0', 'Area code cannot start with zero'],
+  [phoneNumber => phoneNumber[1] === '1', 'Area code cannot start with one'],
+  [phoneNumber => phoneNumber[4] === '0', 'Exchange code cannot start with zero'],
+  [phoneNumber => phoneNumber[4] === '1', 'Exchange code cannot start with one'],
+
+]
+
 /**
  * Clean phone number according to North American Numbering Plan
  * @param {String} number
  * @return {String} NANP number
  */
-export const clean = number => {
-  if (number.match(/[a-zA-Z]/)) {
-    throw new Error('Letters not permitted')
-  }
+export const clean = phoneNumber => {
+  const cleaned = phoneNumber.replace(/[\s\(\)\-\.\+]/g, '')
+  const normalized = cleaned.length === 10 ? '1' + cleaned : cleaned
 
-  if (number.match(/(@:!)/)) {
-    throw new Error('Punctuations not permitted')
-  }
+  VALIDATIONS.forEach(([condition, error]) => {
+    if (condition(normalized)) throw new Error(error)
+  })
 
-  let numbers = Array.from(number.matchAll(/[0-9]/g)).flat().join('')
-
-  if (numbers.length < 10) {
-    throw new Error('Incorrect number of digits')
-  }
-
-  if (numbers.length > 11) {
-    throw new Error('More than 11 digits')
-  }
-
-  if (numbers.length === 11 && numbers[0] != '1') {
-    throw new Error('11 digits must start with 1')
-  }
-
-  if (numbers.length === 11 && numbers[0] === '1') {
-    numbers = numbers.slice(1)
-  }
-
-  if (numbers[0] === '0') {
-    throw new Error('Area code cannot start with zero')
-  }
-
-  if (numbers[0] === '1') {
-    throw new Error('Area code cannot start with one')
-  }
-
-  if (numbers[3] === '0') {
-    throw new Error('Exchange code cannot start with zero')
-  }
-
-  if (numbers[3] === '1') {
-    throw new Error('Exchange code cannot start with one')
-  }
-
-  return numbers
+  return normalized.slice(1)
 }
 
