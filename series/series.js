0c80b697ebe33b3814d33d9fcbe3a5ad70b51a00
Ari Moilanen <armo@iki.fi>
2022-05-28 07:23:20 +0300

    Add series - Iteration 1

--- series/series.js
+++ series/series.js
@@ -1,14 +1,57 @@
-//
-// This is only a SKELETON file for the 'Series' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
+/**
+ * Return all series of slicelength in the string.
+ * @param {String} series
+ */
 export class Series {
   constructor(series) {
-    throw new Error('Remove this statement and implement this function');
+    this.series = series
+    this.seriesLength = series.length
   }
 
+
+  /**
+   * Slices the string to parts of sliceLength
+   * @param {Number} sliceLength
+   * @returns {Array[Integer]}
+   */
   slices(sliceLength) {
-    throw new Error('Remove this statement and implement this function');
+    if (this.seriesLength < 1) {
+      throw new Error('series cannot be empty')
+    }
+
+    this.validate(sliceLength)
+
+    let allSeries = []
+
+    for (let i = 0; i < this.seriesLength - sliceLength + 1; i++) {
+      allSeries
+        .push(this.series.substring(i, (i + sliceLength))
+        .split('')
+        .map(Number))
+    }
+
+    return allSeries
+  }
+
+
+  /** Validates slice length parameter and errors accordingly
+   * @param {Integer} sliceLength
+   */
+  validate(sliceLength) {
+    const VALIDATIONS = [
+      [sliceLength => sliceLength > this.seriesLength,
+        'slice length cannot be greater than series length'],
+      [sliceLength => sliceLength  === 0,
+        'slice length cannot be zero'],
+      [sliceLength => sliceLength < 0,
+        'slice length cannot be negative']
+    ]
+
+    VALIDATIONS.forEach(([condition, error]) => {
+      if (condition(sliceLength)) {
+        throw new Error(error)
+      }
+    })
   }
 }
+
